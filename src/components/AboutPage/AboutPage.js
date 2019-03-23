39eddf7730837ba83d68332e906c6f24abfc2030
Ck T <chengkou@gmail.com>
2019-03-23 12:46:42 -0500

    add teacher page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,18 +1,144 @@
-import React from 'react';
+// import React from 'react';
 
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../App/App.css'
+import Button from '@material-ui/core/Button';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+// import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+// import AlertTemplate from 'react-alert-template-basic'
+// import { useAlert } from 'react-alert'
+import { createMuiTheme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import cyan from '@material-ui/core/colors/cyan';
+import red from '@material-ui/core/colors/red';
+
+// import AddButton from './AddButton';
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
+const theme = createMuiTheme({
+  palette: {
+    primary: cyan,
+    secondary: red,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
 
-const AboutPage = () => (
-  <div>
-    <div>
-      <p>
-        <h1>UNDER CONSTRUCTION</h1>
-      </p>
-    </div>
-  </div>
-);
+  },
+  typography: {
+    useNextVariants: true,
+  }
+});
 
-export default AboutPage;
+class AboutPage extends Component {
+  state = {
+    title: '',
+    date_completed: '',
+    initial: '',
+    bookId: '',
+    user: this.props.user.id,
+    count: this.props.bookReducer.length,
+    isEnable: true,
+
+  };
+
+  //  
+
+
+
+  
+  componentDidMount() {
+    this.fetchBook();
+  }
+  fetchBook = () => {
+    //make call to server using sagas
+    console.log('going to get, title, date, initial');
+    this.props.dispatch({ type: 'FETCH_BOOK' });
+  }
+
+  handleDelete = id => () => {
+    console.log('STUDENT INFO HANDLE DELETE', id);
+    alert('ARE YOU SURE YOU WANT TO DELETE THIS BOOK?, PLEASE CONFIRM!')
+    this.props.dispatch({ type: 'DELETE_BOOK', payload: id })
+    this.setState({
+      title: '',
+      date_completed: '',
+      initial: '',
+      bookId: '',
+      user: this.props.user.id,
+      count: '',
+      isEnable: true,
+    })
+  }
+
+
+  render() {
+    console.log('TJ', this.props.bookReducer);
+    console.log('STATE', this.state);
+
+
+    return (
+      <div >
+        <MuiThemeProvider theme={theme}>
+          <div className="bodyTeacher">
+            </div>
+            <h1>Total books read: {this.props.bookReducer.length}</h1>      
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Book title</th>
+                <th>Initial by a Parent</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.bookReducer.map((bookItem) => {
+                return (
+
+                  <tr key={bookItem.id}>
+                    <td>
+                      {new Date(bookItem.date_completed).getMonth() + 1}/
+                      {new Date(bookItem.date_completed).getDate()}/
+                      {new Date(bookItem.date_completed).getFullYear()}
+                    </td>
+                    <td>
+                      {bookItem.title}
+                    </td>
+                    <td>
+                      {bookItem.initial}
+                    </td>
+                    <td>
+                      <Button variant="contained" color="secondary" onClick={this.handleDelete(bookItem.book_id_id)} disabled={this.state.isEnable}>Delete</Button>
+                    </td>
+                  </tr>
+                )
+              }
+              )}
+            </tbody>
+          </table>
+        </MuiThemeProvider>
+      </div>
+
+    );
+  }
+}
+
+const mapStateToProps = reduxState => {
+  return reduxState
+};
+export default (connect(mapStateToProps)(AboutPage));
+
+// const AboutPage = () => (
+//   <div>
+//     <div>
+//       <p>
+//         <h1>UNDER CONSTRUCTION</h1>
+//       </p>
+//     </div>
+//   </div>
+// );
+
+// export default AboutPage;
