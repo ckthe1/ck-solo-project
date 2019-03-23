a4c3924a21b51c88af12eaa451df0e7201fd7bc9
Ck T <chengkou@gmail.com>
2019-03-23 09:55:38 -0500

    add comment in book-router

--- server/routes/book.router.js
+++ server/routes/book.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 let dataIds = [];
+//insert into date table, title table, and all id's in relationship table
 router.post('/',(req,res)=>{
     console.log('req.body:', req.body);
     
@@ -28,7 +29,7 @@ router.post('/',(req,res)=>{
                 console.log('dataIds:', dataIds);
                 const newBook = req.body;
                 const queryText = `INSERT INTO "relationship" ("date_id","book_id","initial", "student_id")
-                                VALUES ($1,$2,$3,$4) RETURNING id`;
+                                    VALUES ($1,$2,$3,$4) RETURNING id`;
                 const queryValues = [
                     dataIds[dataIds.length-2],
                     dataIds[dataIds.length - 1],
@@ -50,10 +51,10 @@ router.get('/', (req, res) => {
     console.log('req.user.id', req.user.id);
     
     const queryText = (`SELECT  "title", "date_completed","initial", "date"."id" AS "date_completed_id", "books"."id" AS "book_id_id" FROM "date"
-                JOIN "relationship" ON "date"."id" = "relationship"."date_id"
-                JOIN "user" ON "user"."id" = "relationship"."student_id"
-                JOIN "books" ON "books"."id" = "relationship"."book_id"
-                WHERE "user"."id" = $1;`               
+                        JOIN "relationship" ON "date"."id" = "relationship"."date_id"
+                        JOIN "user" ON "user"."id" = "relationship"."student_id"
+                        JOIN "books" ON "books"."id" = "relationship"."book_id"
+                        WHERE "user"."id" = $1;`               
                 )
         pool.query(queryText,[req.user.id]).then((result) => {
             res.send(result.rows)
@@ -65,6 +66,7 @@ router.get('/', (req, res) => {
     })
 });
 
+// delete from relationship table, from date table and books table
 router.delete('/:id', (req,res)=>{
 
     console.log('THESE ARE MY REQ.PARAMS', req.params);   
